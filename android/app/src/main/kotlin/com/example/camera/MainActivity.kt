51f31897930a09bf6043e7274f444f9a1f29a42d
Urip Yogantara <uripyogantara@gmail.com>
2019-11-01 14:39:27 +0700

    response from native to flutter

--- android/app/src/main/kotlin/com/example/camera/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/camera/MainActivity.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.support.v4.content.FileProvider
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugin.common.MethodChannel
@@ -13,14 +14,26 @@ import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
+import android.R.attr.data
+import android.app.Activity
+import android.support.v4.app.NotificationCompat.getExtras
+import android.graphics.Bitmap
+import android.support.v4.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.support.v4.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.support.v4.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.support.v4.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import java.io.ByteArrayOutputStream
+
 
 class MainActivity: FlutterActivity() {
   private lateinit var methodResult: MethodChannel.Result
 
   private val CHANNEL = "camera.flutter.io/camera"
   private val METHOD_CAMERA= "getCamera"
-
-  private lateinit var imageFilePath:String
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -30,57 +43,34 @@ class MainActivity: FlutterActivity() {
       when (call.method) {
         METHOD_CAMERA->{
           methodResult=result
-          takePicture()
+          openCameraIntent()
         }
       }
     }
   }
 
-  private val TAKE_PICTURE = 1
+  private val REQUEST_CAPTURE_IMAGE = 100
 
-  fun takePicture(){
-    val takePictureIntent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-
-    if(takePictureIntent.resolveActivity(getPackageManager()) != null){
-
-      var photoFile: File? = null
-      try {
-        photoFile =createImageFile()
-      }catch (e: IOException){
-//      Log.d(LOG_TAG,e.printStackTrace().toString())
-      }
-
-      if(photoFile !=null) {
-//        val photoUri: Uri = FileProvider.getUriForFile(this,"com.example.image_upload.fileprovider",photoFile)
-//        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,photoUri)
-        startActivityForResult(takePictureIntent,TAKE_PICTURE)
-      }
-    }
-  }
-
-  @Throws(IOException::class)
-  private fun createImageFile(): File {
-    val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss",
-            Locale.getDefault()).format(Date())
-    val imageFileName = "IMG_" + timeStamp + "_"
-    val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-    val image = File.createTempFile(
-            imageFileName, /* prefix */
-            ".jpg", /* suffix */
-            storageDir      /* directory */
+  private fun openCameraIntent() {
+    val pictureIntent = Intent(
+            MediaStore.ACTION_IMAGE_CAPTURE
     )
 
-    imageFilePath = image.absolutePath
-    return image
+    startActivityForResult(pictureIntent, REQUEST_CAPTURE_IMAGE)
   }
 
-  override fun onActivityResult(requestCode: Int,
-                                resultCode: Int,
-                                data: Intent) {
-
-    if (requestCode == TAKE_PICTURE) {
-      methodResult.success("suksesss fotonya")
-
+  override fun onActivityResult(requestCode: Int, resultCode: Int,
+                                data: Intent?) {
+    if (requestCode == REQUEST_CAPTURE_IMAGE) {
+      if (data != null && data.extras != null) {
+          val bitmap = data.extras!!.get("data") as Bitmap
+          val stream = ByteArrayOutputStream()
+          bitmap.compress(Bitmap.CompressFormat.JPEG, 80, stream)
+          val byteArray = stream.toByteArray()
+          methodResult.success(byteArray)
+      }
     }
   }
+
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -29,25 +31,57 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   static const platform = const MethodChannel("camera.flutter.io/camera");
-
+  Uint8List _image;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: RaisedButton(
-          child: Text("Open Camera"),
-          onPressed: () async{
-            await platform.invokeMethod("getCamera").then((stringResponse){
+      body:
+          _formUpload(), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
 
-              print("response camera $stringResponse");
-              // Map map = json.decode(stringResponse);
-              // print(map);
-            });
-        },),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+  Widget _formUpload() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: Column(
+        children: <Widget>[
+          Container(
+            child: _image == null
+                ? Text('No Images Selected')
+                : Image.memory(_image),
+          ),
+          SizedBox(
+            height: 50.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                child: Icon(Icons.camera),
+                onPressed: () async {
+                  await platform
+                      .invokeMethod("getCamera")
+                      .then((stringResponse) {
+                    setState(() {
+                      _image = stringResponse;
+                    });
+                    // print(input);
+                    // print("response camera $stringResponse");
+                    // setState(() {
+                    //   _image = File.fromRawPath(input);
+                    // });
+                    // Map map = json.decode(stringResponse);
+                    // print(map);
+                  });
+                },
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
